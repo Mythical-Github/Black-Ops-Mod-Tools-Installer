6a5f84d07a4d6393a1e914e7b07562438031eec9
Mythical-Github <67753356+Mythical-Github@users.noreply.github.com>
2021-08-10 03:13:50 -0400

    v1.2.1, updated batch files, & readmes

--- Steam_Install.bat
+++ Steam_Install.bat
@@ -1,6 +1,6 @@
 mkdir "Temp"
 cd "Temp"
-powershell -Command "Start-BitsTransfer -Source https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip,  https://github.com/Nukem9/LinkerMod/releases/download/v1.3.2/game_mod.zip, https://cdn.discordapp.com/attachments/868600140880023592/873732438042505226/BO1_Radiant.zip, https://github.com/Nukem9/LinkerMod/releases/download/v1.0.0-r/LinkerMod-1.0.0.zip, https://cdn.discordapp.com/attachments/868600140880023592/873741090350583818/assets_wip_0.1.0.zip, https://cdn.discordapp.com/attachments/868600140880023592/873718947285979146/CoD_BO_Mod_Tools_Fix_1.4.1.zip, https://cdn.discordapp.com/attachments/868600140880023592/874069375773196338/End_Message.txt"
+powershell -Command "Start-BitsTransfer -Source https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip,  https://github.com/Nukem9/LinkerMod/releases/download/v1.3.2/game_mod.zip, https://cdn.discordapp.com/attachments/868600140880023592/873732438042505226/BO1_Radiant.zip, https://github.com/Nukem9/LinkerMod/releases/download/v1.0.0-r/LinkerMod-1.0.0.zip, https://cdn.discordapp.com/attachments/868600140880023592/873741090350583818/assets_wip_0.1.0.zip, https://cdn.discordapp.com/attachments/868600140880023592/873718947285979146/CoD_BO_Mod_Tools_Fix_1.4.1.zip, https://cdn.discordapp.com/attachments/868600140880023592/874550119742205982/End_Message.txt"
 powershell -Command "Expand-Archive -Force -LiteralPath steamcmd.zip"
 cd ".."
 call "Temp/steamcmd/steamcmd.exe" +login "YOURUSERNAME" "YOURPASSWORD" +force_install_dir "../.." +app_update 42700 +app_update 42740 verify +exit
@@ -11,12 +11,12 @@ powershell -Command "Expand-Archive -Force -LiteralPath BO1_Radiant.zip -Destina
 start steam://rungameid/42700
 Timeout /T 90
 taskkill /f /im BlackOps.exe
-cd: .."
+cd ".."
 cd "bin\scripts"
 call setup.bat
 cd ".."
 call converter.exe -nopause -n -nospam
-d ".."
+cd ".."
 cd "Temp"
 powershell -Command "Expand-Archive -Force -LiteralPath assets_wip_0.1.0.zip -DestinationPath "..""
 powershell -Command "Expand-Archive -Force -LiteralPath CoD_BO_Mod_Tools_Fix_1.4.1.zip -DestinationPath "..""
@@ -24,3 +24,4 @@ type End_Message.txt
 pause 30
 cd ".."
 powershell -Command "Remove-Item -Force -Recurse "Temp""
+powershell -Command "Remove-Item -Force -Recurse "steamapps""
